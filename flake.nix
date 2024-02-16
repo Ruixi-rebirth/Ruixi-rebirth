e9b5dc523680bc1aa17cc81c68152b11ec527d71
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-02-17 03:53:08 +0800

    update

--- flake.nix
+++ flake.nix
@@ -3,13 +3,26 @@
     emanote.url = "github:srid/emanote";
     nixpkgs.follows = "emanote/nixpkgs";
     flake-parts.follows = "emanote/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.emanote.flakeModule ];
-      perSystem = { self', pkgs, system, ... }: {
+      imports = [
+        inputs.emanote.flakeModule
+        inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+      ];
+      perSystem = { self', pkgs, system, config, ... }: {
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+          programs.nixpkgs-fmt.enable = true;
+          programs.prettier.enable = true;
+        };
+
         emanote = {
           sites."default" = {
             layers = [ ./. ];
@@ -21,10 +34,10 @@
         };
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.nixpkgs-fmt
+            inputs.emanote.packages.${pkgs.system}.emanote
           ];
         };
-        formatter = pkgs.nixpkgs-fmt;
+        formatter = config.treefmt.build.wrapper;
       };
     };
 }
